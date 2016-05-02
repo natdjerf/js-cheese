8b894a7eef957bb2bbf3ae7c4c414a6eb62e1993
natdjerf <nadjerf@gmail.com>
2016-05-02 11:59:38 -0400

    Scroll to section buttons & jquery.

--- assets/scripts/auth/cheese-events.js
+++ assets/scripts/auth/cheese-events.js
@@ -32,7 +32,17 @@ const ui = require('./user-ui');
 //   });
 // };
 
-
+// scroll function
+function scrollToID(id, speed){
+	let offSet = 50;
+	let targetOffset = $(id).offset().top - offSet;
+	let mainNav = $('#main-nav');
+	$('html,body').animate({scrollTop:targetOffset}, speed);
+	if (mainNav.hasClass("open")) {
+		mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+		mainNav.removeClass("open");
+	}
+}
 
 
 const cheeseHandlers = () => {
@@ -66,17 +76,30 @@ const cheeseHandlers = () => {
    console.log(data);
    // data = the details of the board/cheese ids to the cheese addition table
    cheeseApi.addToBoard(cheeseUi.addToBoardSuccess,cheeseUi.addToBoardFailure, data);
-});
+});	// navigation click actions
+	$('.scroll-link').on('click', function(event){
+		event.preventDefault();
+		var sectionID = $(this).attr("data-id");
+		scrollToID('#' + sectionID, 750);
+	});
+	// scroll to top action
+	$('.scroll-top').on('click', function(event) {
+		event.preventDefault();
+		$('html, body').animate({scrollTop:0}, 'slow');
+	});
+	// mobile nav toggle
+	$('#nav-toggle').on('click', function (event) {
+		event.preventDefault();
+		$('#main-nav').toggleClass("open");
+	});
 
 };
 
 
 
 
-
-
-
 module.exports = {
     // getCheeses,
-    cheeseHandlers
+    cheeseHandlers,
+    scrollToID
   };
